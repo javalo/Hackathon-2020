a9f24b6759fd5b9101e3e2c715252d4da4fba451
javalo <lo.cheikhna@gmail.com>
2020-12-17 00:46:00 +0100

    design NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Nav from "./components/navBar/Nav";
+import Home from "./components/home/home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Nav></Nav>
+
+  <Switch>
+
+    <Route exact from="/" render={props => <Home />} />
+
+    <Route exact path="/contact" render={props => <Home {...props} />}  />
+</Switch>
+
     </div>
   );
 }
